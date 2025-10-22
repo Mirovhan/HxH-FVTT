b79451d3b0406bcfc20e53d16c3a78a574e5e7dc
Mirovhan <gcevallosg@gmail.com>
2025-10-22 14:21:01 -0300

    v1.2.0

--- module/hxh-fvtt.js
+++ module/hxh-fvtt.js
@@ -1,12 +1,1 @@
-import { HxHActor } from "./actor/actor.js";
-import { HxHCharacterSheet } from "./actor/sheets/character-sheet.js";
-
-Hooks.once("init", async function() {
-  CONFIG.Actor.documentClass = HxHActor;
-  Actors.registerSheet("hxh-fvtt", HxHCharacterSheet, { types: ["character"], makeDefault: true });
-
-  Handlebars.registerHelper("eq", (a, b) => a === b);
-  await loadTemplates([
-    "systems/hxh-fvtt/templates/actor/character-sheet.hbs"
-  ]);
-});
+console.log('HxH FVTT v1.2.0 loaded');
